b1cb2d0f542577fab7bc6c942fc8ba265bd79981
Athymist <alexandra.mirakoff@gmail.com>
2014-05-13 20:46:23 +0200

    ... bref.

--- wp-content/themes/html5blank-master/index.php
+++ wp-content/themes/html5blank-master/index.php
@@ -14,6 +14,22 @@
 		<!-- /section -->
 	</main>
 
+<body>
+<div id="wb_element_instance0" class="wb_element" style="left: 0px; top: 29px; width: 898px; height: 51px; min-width: 898px; min-height: 51px; z-index: 46;">
+	<ul class="hmenu">
+		<li class="active">
+			<a href="accueil.html" target="_self" title="Accueil du site">Accueil</a>
+		</li>
+		<li>
+			<a href="societe.html" target="_self" title="Notre société">Le centre</a>
+		</li>
+		<li><a href="contact.html" target="_self" title="Contacts">Contact</a>
+		</li>
+	</ul>
+</div>
+
+</body>
+
 <?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
